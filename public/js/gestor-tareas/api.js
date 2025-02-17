fde588f8521272222dbd42954acdf7ada4093765
IlloBravo <bravodearcedavid@gmail.com>
2025-02-17 12:14:41 +0100

    fix code for api js

--- public/js/gestor-tareas/api.js
+++ public/js/gestor-tareas/api.js
@@ -1,13 +1,22 @@
 export function fetchTasks(callback) {
-    $.get("/get-all-tasks", function (data) {
-        callback(data);
-    });
+    $.ajax({
+        url: "/get-all-tasks",
+        type: "GET",
+        dataType: "json"
+    })
+        .done(callback)
+        .fail(error => console.error("Error fetching tasks:", error));
 }
 
 export function createTask(taskData, successCallback, errorCallback) {
-    $.post("/create-tasks", {
-        ...taskData,
-        _token: $("meta[name='csrf-token']").attr("content")
+    $.ajax({
+        url: "/create-tasks",
+        type: "POST",
+        data: {
+            ...taskData,
+            _token: $("meta[name='csrf-token']").attr("content")
+        },
+        dataType: "json"
     })
         .done(successCallback)
         .fail(errorCallback);
@@ -17,7 +26,8 @@ export function deleteTask(taskId, successCallback, errorCallback) {
     $.ajax({
         url: `/tasks/${taskId}`,
         type: "DELETE",
-        data: { _token: $("meta[name='csrf-token']").attr("content") }
+        data: { _token: $("meta[name='csrf-token']").attr("content") },
+        dataType: "json"
     })
         .done(successCallback)
         .fail(errorCallback);
